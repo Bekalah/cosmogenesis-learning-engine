4b4e053f25639b57971e62087cefae1a73225864
Rebecca Respawn <62661983+Bekalah@users.noreply.github.com>
2025-08-30 01:01:44 -0500

    Create seal.js

---
+++ app/experience_c/components/seal.js
@@ -0,0 +1,4 @@
++export default function(engine){
++  engine.setConfig({labels:['Earth','Water','Air','Fire','Spirit','Sulfur','Mercury']}).render();
++  window.toast?.('Agrippa sketches planetary seals.');
++}
