97de454a08aeed060ed4f99cf13a0bf12c403b72
Rebecca Respawn <62661983+Bekalah@users.noreply.github.com>
2025-08-30 00:06:52 -0500

    Create counter.js

---
+++ experiences/tesla/components/counter.js
@@ -0,0 +1,10 @@
++export default function mount(el) {
++  let volts = 0;
++  const btn = document.createElement('button');
++  btn.textContent = `Tesla volts: ${volts}`;
++  btn.addEventListener('click', () => {
++    volts++;
++    btn.textContent = `Tesla volts: ${volts}`;
++  });
++  el.appendChild(btn);
++}
