fc8f538e7f32dcd8714a2e8f3e3ef1040c17f8d1
Rebecca Respawn <62661983+Bekalah@users.noreply.github.com>
2025-08-30 00:00:59 -0500

    Create counter.js

---
+++ experiences/alexandrian-scriptorium/components/counter.js
@@ -0,0 +1,10 @@
++export default function mount(el) {
++  let scrolls = 0;
++  const btn = document.createElement('button');
++  btn.textContent = `Scriptorium scrolls: ${scrolls}`;
++  btn.addEventListener('click', () => {
++    scrolls++;
++    btn.textContent = `Scriptorium scrolls: ${scrolls}`;
++  });
++  el.appendChild(btn);
++}
