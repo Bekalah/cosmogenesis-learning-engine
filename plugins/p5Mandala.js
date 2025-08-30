60c47191d6dfe2dd866f2f46bd68ebe09bf093cd
Rebecca Respawn <62661983+Bekalah@users.noreply.github.com>
2025-08-30 12:58:26 -0500

    Update p5Mandala.js

--- plugins/p5Mandala.js
+++ plugins/p5Mandala.js
@@ -1,22 +1,23 @@
-+export default async function(){
-+  if(!window.p5){
-+    try{
-+      await import('../vendor/p5.min.js');
-+    }catch{
-+      await import('https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.min.js');
-+    }
-+  }
-+  new p5(p=>{
-+    p.setup = ()=>{ p.createCanvas(320,320); p.angleMode(p.DEGREES); p.noFill(); };
-+    p.draw = ()=>{
-+      p.background(0,0,0,10);
-+      p.translate(p.width/2,p.height/2);
-+      for(let i=0;i<12;i++){
-+        p.push();
-+        p.rotate((p.frameCount/2)+i*30);
-+        p.stroke(255,150);
-+        p.ellipse(0,40,20,20);
-+        p.pop();
-+      }
-+    };
-+  });
+export default async function p5Mandala(){
+  if(!window.p5){
+    try{
+      await import('../vendor/p5.min.js');
+    }catch{
+      await import('https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.min.js');
+    }
+  }
+  new p5(p=>{
+    p.setup = ()=>{ p.createCanvas(320,320); p.angleMode(p.DEGREES); p.noFill(); };
+    p.draw = ()=>{
+      p.background(0,0,0,10);
+      p.translate(p.width/2,p.height/2);
+      for(let i=0;i<12;i++){
+        p.push();
+        p.rotate((p.frameCount/2)+i*30);
+        p.stroke(255,150);
+        p.ellipse(0,40,20,20);
+        p.pop();
+      }
+    };
+  });
+}
